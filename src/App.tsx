ef693a7330658adcccf71517dcb129c173d69d95
Nagendra Mishra <beetu111.mishra@gmail.com>
2021-10-09 13:33:13 +0800

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="container">
+    <h2>Pizza Mania</h2>
+    </div>
+  );
+}
+
+export default App;
